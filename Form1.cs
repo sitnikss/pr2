3e5596b24a7c4b6834ce907b526745246fee7fc5
Stanislav <sit.stas14@mail.ru>
2020-11-21 11:59:33 +0900

    polkoda

--- Form1.cs
+++ Form1.cs
@@ -24,9 +24,18 @@ namespace WindowsFormsApp7
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var str = textBox1.Text;
 
+            var tuple = Check(str);
+            if (tuple.Item1)
+                textBox2.Text = ("Правильность расстановки скобок правильная.");
+            else if (tuple.Item2 >= 0)
+                textBox2.Text = ($"В позизиции {tuple.Item2} обнаружена непарная закрывающая скобка!");
+            else
+                textBox2.Text = ($"В строке не хватает {tuple.Item3} закрывающих скобок!");
         }
 
+        }
         private void button2_Click(object sender, EventArgs e)
         {
 
